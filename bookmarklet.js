dd121e1aa257d279765726d769aef9ed04ea54e5
Gaëtan RENAUDEAU <renaudeau.gaetan@gmail.com>
2011-07-28 11:43:35 +0200

    fix bookmarklet magic

--- bookmarklet.js
+++ bookmarklet.js
@@ -4,7 +4,7 @@
  */
 
 (function(){
-  var base = window.flexibleNavBase || 'https://github.com/gre/flexible-nav/raw/master/';
+  var base = window.flexibleNavBase;
   fullFunc({
     css : [ base + "lib/flexible-nav.min.css"],
     js  : [ base + "lib/flexible-nav.min.js" ],
@@ -20,7 +20,7 @@
 // ... http://www.latentmotion.com/downloads/blank-bookmarklet-v1.js
 function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}
 $.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>")
-.attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.
+.attr({href:f,rel:"stylesheet",type:'text/css'}).appendTo("head")})}a.jqpath=a.
 jqpath||"http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js";
 (function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;
 c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
